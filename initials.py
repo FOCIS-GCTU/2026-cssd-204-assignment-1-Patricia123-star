7afde441dedfe0381af2966410b5c1b003aaad03
Patricia123-star <bedzrahpatricia@gmail.com>
2026-05-31 19:04:49 +0000

    initials to my name

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials
+# Assignment number: 1
+#
+# Name:          Bedzrah Patricia Ewoenam
+# STUDENT ID:    2425400600
+# Email:         2425400600@live.gctu.edu.gh
+# Grader:        Augustus
+#
+# On my honor, Bedzrah Patricia Ewoenam, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    print("")
 
+    print("...BPE")
 
+    print("")
+
+    print("...BBBBBBBB............PPPPPPPPP...........EEEEEEEEEEEE.....")
+    print("...BBBBBBBBBBB.........PPPPPPPPPPP.........EEEEEEEEEEEE.....")
+    print("...BB........BB........PP........PP........EE...............")
+    print("...BB........BB........PP........PP........EE...............")
+    print("...BBBBBBBBBBB.........PPPPPPPPPPP.........EEEEEEEEEEEE.....")
+    print("...BBBBBBBBB...........PPPPPPPPP...........EEEEEEEEEEEE.....")
+    print("...BB........BB........PP..................EE...............")
+    print("...BB........BB........PP..................EE...............")
+    print("...BBBBBBBBBBB....**...PP.............**...EEEEEEEEEEEE...**")
+    print("...BBBBBBBB.......**...PP.............**...EEEEEEEEEEEE...**")
+
+    print("")
 
 main()
